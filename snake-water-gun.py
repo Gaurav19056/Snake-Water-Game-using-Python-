b6e84653f00e1816d2fbea17e079068c8e5a48ea
Gaurav Drall <drallgaurav90@gmail.com>
2025-07-27 13:36:24 +0530

    Update snake-water-gun.py
    added line 3,4 and 6

--- snake-water-gun.py
+++ snake-water-gun.py
@@ -1,6 +1,9 @@
 import random
 #0--> snake.....1-->water......2-->gun
+print("---")
+print("Welcome to Snake, Water, Gun!")
 print("Rules: Snake drinks Water, Water douses Gun, Gun kills Snake.")
+print("---")
 computer=random.choice([0,1,2])
 your_choice=int(input("0 for snake,1 for water,2 for gun: "))
 if(your_choice==computer):
